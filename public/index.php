e9bc186c994c7bfbc3a5c344d851b3b366f053dd
Till Wehowski <github@webfan.de>
2022-10-27 23:16:51 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2843,7 +2843,6 @@ Content-Type: application/x-httpd-php
 	}
 
 	return array (
-   //'appId' => 'oid:1.3.6.1.4.1.37553.8.1.8.8.1958965301',
    /****'appId'=>'@@@APPID@@@',*****/	
   'time' => 0,
   'version' => '0.0.0',

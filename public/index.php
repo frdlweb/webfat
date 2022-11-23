ed6a09b4c9b2bf9439a52a4bea488bbe7850336a
Till Wehowski <github@webfan.de>
2022-11-23 01:28:38 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -151,6 +151,49 @@ interface ContainerInterface
 
 namespace frdl\booting{
 	
+use Spatie\Once\Backtrace;
+use Spatie\Once\Cache;
+
+/**
+ * once by:
+ * https://github.com/spatie/once
+ */
+function once(callable $callback): mixed
+{
+    $trace = debug_backtrace(
+        DEBUG_BACKTRACE_PROVIDE_OBJECT, 2
+    );
+
+    $backtrace = new Backtrace($trace);
+
+    if ($backtrace->getFunctionName() === 'eval') {
+        return call_user_func($callback);
+    }
+
+    $object = $backtrace->getObject();
+
+    $hash = $backtrace->getHash();
+
+    $cache = Cache::getInstance();
+
+    if (is_string($object)) {
+        $object = $cache;
+    }
+
+    if (! $cache->isEnabled()) {
+        return call_user_func($callback, $backtrace->getArguments());
+    }
+
+    if (! $cache->has($object, $hash)) {
+        $result = call_user_func($callback, $backtrace->getArguments());
+
+        $cache->set($object, $hash, $result);
+    }
+
+    return $cache->get($object, $hash);
+}
+	
+	
  $maxExecutionTime = intval(ini_get('max_execution_time'));	
  if (strtolower(\php_sapi_name()) !== 'cli') {	 
     @set_time_limit(180);

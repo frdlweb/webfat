c02c2b07f714eab76ca4c3ca59bd050ad029208b
Till Wehowski <github@webfan.de>
2023-07-30 12:24:01 +0200

    Create index.php

---
+++ install/stub/index.php
@@ -0,0 +1,17 @@
+<?php 
+	
+    $this->getRunner()->init();
+ 	$AppLauncher = new \Webfan\AppLauncherWebfatInstaller($this->getRunner()); 
+ 	 if(\method_exists($AppLauncher, 'launch')){
+	   $AppLauncher->launch();
+	}elseif(!$AppLauncher->KernelFunctions()->isCLI() ){
+		   $response = $AppLauncher->handle($AppLauncher->getContainer()->get('request'));
+		   if(is_object($response) && $response instanceof \Psr\Http\Message\ResponseInterface){ 
+			   (new \Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
+		   }
+	 
+	   }elseif($AppLauncher->KernelFunctions()->isCLI() ){
+		 return $AppLauncher->handleCliRequest();
+	   }else{
+	     throw new \Exception('Could not handle request ('.\PHP_SAPI.')');	
+       }	

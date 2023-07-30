c1125e674779dacfe3847b9f52b4c834e747c67d
Till Wehowski <github@webfan.de>
2023-07-30 12:41:20 +0200

    Update alias.php

--- modules/io4/facades/container/alias.php
+++ modules/io4/facades/container/alias.php
@@ -4,7 +4,6 @@
 return [
    'fascades.helper' =>( function(\Psr\Container\ContainerInterface $container){
 	      return \Webfan\FacadeProxiesMap::createProxy([
-		        new \App\Testy\Helper(), 
 		        new \Webfan\Webfat\App\KernelHelper,
 		        new \Webfan\Webfat\App\KernelFunctions,
 		     ],

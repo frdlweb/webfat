02a6d898d9ad222a3091a2751c9cf73691584a28
Till Wehowski <github@webfan.de>
2023-07-30 11:45:16 +0200

    Delete webfan.config.php

--- webfan.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$module['exports'] = function(?\Webfan\Wayne\Insaneable ...$args){
-  return (new class(...$args){
-         public function __construct(?\Webfan\Wayne\Insaneable ...$args){
-           
-         }
-  });
-};

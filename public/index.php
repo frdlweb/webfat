accf12d34f568af79c160b42c969b58a5575b852
Till Wehowski <github@webfan.de>
2023-09-01 17:49:34 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4763,7 +4763,7 @@ putenv('FRDL_HPS_PSR4_CACHE_DIR='.$_ENV['FRDL_HPS_PSR4_CACHE_DIR']);
 		   ))->setTimeout( 60 ) );
 
 
-		$this['Container']->setCall([$this, 'call']);
+		//$this['Container']->setCall([$this, 'call']);
 		$this['Container']->set(\IO4\Container\ContainerCollectionInterface::CALL_ID, [$this, 'call']);		
 
 		
